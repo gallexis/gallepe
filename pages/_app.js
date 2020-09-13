596aeabac6e619243249620b27405a303e043b27
gallexis <alexis.gallepe@gmail.com>
2020-09-13 13:34:45 +0200

    update analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import ReactGA from 'react-ga';
+import App from 'next/app'
+
+class MyApp extends App {
+  componentDidMount() {
+    ReactGA.initialize('UA-177859198-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+     <Component {...pageProps} />
+    )
+  }
 }
 
 export default MyApp
