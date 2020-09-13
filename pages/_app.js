c5873ba69020ec7912ef13acaa98a5b23c7d7b9d
gallexis <alexis.gallepe@gmail.com>
2020-09-13 13:37:24 +0200

    update analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,15 @@ import ReactGA from 'react-ga';
 import App from 'next/app'
 
 class MyApp extends App {
+
   componentDidMount() {
-    ReactGA.initialize('UA-177859198-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.initialize("UA-177859198-1", { debug: false });
+    ReactGA.set({ page: this.props.router.pathname });
+    ReactGA.pageview(this.props.router.pathname);
+    this.unlisten = this.props.router.events.on("routeChangeComplete", (router) => {
+      ReactGA.set({ page: router });
+      ReactGA.pageview(router);
+    });
   }
 
   render() {
