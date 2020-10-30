abece8fdcaccbbf404c05271c2e591c0653f3aec
gallexis <alexis.gallepe@gmail.com>
2020-10-30 13:45:33 +0100

    update microsoft clarity code

--- components/Layout.js
+++ components/Layout.js
@@ -52,7 +52,7 @@ export default function Layout({ children, title, page }) {
                         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "3vxhjw1vne");
+                    })(window, document, "clarity", "script", "3w169yr8bf");
                   }
               })(window, document);
         `,
