9cc42d0fc9efbc9540430028ffc6e0dcf011507e
gallexis <alexis.gallepe@gmail.com>
2020-10-30 13:24:03 +0100

    add microsoft clarity

--- components/Layout.js
+++ components/Layout.js
@@ -46,11 +46,15 @@ export default function Layout({ children, title, page }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "3vxhjw1vne");
+              (function(window, document){
+                  if (window.location.host ==='gallepe.com'){
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "3vxhjw1vne");
+                  }
+              })(window, document);
         `,
           }}
         />
