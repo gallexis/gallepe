9a996bcec841fb467e9ea8c19d86afa463d536ce
gallexis <alexis.gallepe@gmail.com>
2021-04-11 09:59:24 +0200

    assets optimizations

--- js/custom.js
+++ js/custom.js
@@ -4,14 +4,14 @@ $(window).on('load', function() {
     /*=========================================================================
         Preloader
     =========================================================================*/
-    $("#preloader").delay(350).fadeOut('slow');
-    // Because only Chrome supports offset-path, feGaussianBlur for now
-    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-
-    if(!isChrome) {
-        document.getElementsByClassName('infinityChrome')[0].style.display = "none";
-        document.getElementsByClassName('infinity')[0].style.display = "block";
-    }
+    // $("#preloader").delay(350).fadeOut('slow');
+    // // Because only Chrome supports offset-path, feGaussianBlur for now
+    // var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    //
+    // if(!isChrome) {
+    //     document.getElementsByClassName('infinityChrome')[0].style.display = "none";
+    //     document.getElementsByClassName('infinity')[0].style.display = "block";
+    // }
 
     /*=========================================================================
      Wow Initialize
